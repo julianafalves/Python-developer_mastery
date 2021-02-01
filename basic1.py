38b4e1c67b800e7799f2a8d109f110a99b7ef2ac
Juliana F. Alves <julianaferreira@estudante.ufscar.br>
2021-02-01 00:08:33 -0300

    add interesting basic  code

---
+++ basic1.py
@@ -0,0 +1,16 @@
+#Binary
+print(bin(5))
+print(int('0b101',2))
+
+#complex number
+print( complex(3,2))
+
+#escape sequences
+#when you want to use Paragraph, Quotation marks or Apostrophe" inside a string, you must use \ before
+
+print( "\tParagraph  \nQuotation marks  \"example\" \nApostrophe  \'example' ")
+
+#string index
+name = 'juliana'
+print(name[::-1])
+print(name[::2])
